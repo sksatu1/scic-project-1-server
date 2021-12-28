09a6f8d5402bd691f8e2926394284f2d9edef93d
sksatu1 <satueee18@gmail.com>
2021-12-29 00:45:38 +0600

    3rd

--- index.js
+++ index.js
@@ -10,3 +10,132 @@ const port = process.env.PORT || 5000;
 // middleware-----
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kzjok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected');
+
+        const database = client.db('bookstore');
+
+        const booksCollection = database.collection('books');
+        const ordersCollection = database.collection('orders');
+        const reviewsCollection = database.collection('reviews');
+        const usersCollection = database.collection('users');
+
+
+        // get API ----------------------
+        app.get('/books', async (req, res) => {
+            const cursor = booksCollection.find({});
+            const books = await cursor.toArray();
+            res.send(books);
+        })
+
+        app.get('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await booksCollection.findOne(query);
+            console.log('load book with id: ', id);
+            res.send(product);
+        })
+
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const books = await cursor.toArray();
+            res.send(books);
+        })
+
+        // load reviews ------------
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+         // for finding a admin user ---------------------------------
+         app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+
+        // post API -------------------------
+        app.post('/orders', async (req, res) => {
+            const orderInfo = req.body;
+            console.log(orderInfo);
+            const result = await ordersCollection.insertOne(orderInfo);
+            console.log(result);
+            res.json(result);
+        });
+
+        // post API -------------------------
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log(review);
+            const result = await reviewsCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        });
+
+        // POST api------------------------------
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+
+        // PUT api -------------------------------
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+
+        // delete API ------------------------
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log('deleting user with id : ', result);
+            res.json(result);
+        });
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hello from my second node server');
+})
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
